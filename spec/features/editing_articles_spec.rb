80b06c3967b2ed733dd92b82c1c0e6c1a85c9705
Faiz <tenkois@gmail.com>
2018-10-29 23:35:03 +0800

    edit article done

--- spec/features/editing_articles_spec.rb
+++ spec/features/editing_articles_spec.rb
@@ -5,23 +5,25 @@ RSpec.feature "Editing an Article" do
     end
 
     scenario "A user updates an article" do
-    visit "/"
-    click_link @article.title
-    click_link "Edit Article"
-    fill_in "Title", with: "Updated Article"
-    fill_in "Body", with: "Lorem Ipsum"
-    click_button "Update Article"
-    
-    expect(page).to have_content("Article has been updated")
-    expect(page.current_path).to eq(article_path(@article))
+      visit "/"
+      click_link @article.title
+      click_link "Edit Article"
+
+      fill_in "Title", with: "Updated Article"
+      fill_in "Body", with: "Updated Body Of Article"
+      click_button "Update Article"
+
+      expect(page).to have_content("Article has been updated")
+      expect(page.current_path).to eq(article_path(@article))
     end
 
     scenario "A user fails to update an article" do
       visit "/"
-      click_link @article.title click_link "Edit Article"
+      click_link @article.title
+      click_link "Edit Article"
 
       fill_in "Title", with: ""
-      fill_in "Body", with: "Lorem Ipsum"
+      fill_in "Body", with: "Updated Body Of Article"
 
       click_button "Update Article"
       expect(page).to have_content("Article has not been updated")
